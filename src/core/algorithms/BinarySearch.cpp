0c2b188e1fbd36d47bd324f828e8bdbfe56d665a
Emeteil <vorobievdima43@gmail.com>
2026-03-21 12:49:56 +0700

    add fixing QuickSort methods and BinarySearch

--- src/core/algorithms/BinarySearch.cpp
+++ src/core/algorithms/BinarySearch.cpp
@@ -8,4 +8,21 @@ namespace FilmLibrary
     // NOTE: BinarySearch - шаблонный класс. Все методы определены в заголовочном файле.
 
     // TODO: Реализовать бинарный поиск.
+    template <typename T, typename Value, typename ValueGetter>
+    std::vector<T> BinarySearch::FindInRange(const std::vector<T>& sortedData, const Value& low, const Value& high, ValueGetter getter)
+    {
+        return {};
+    }
+
+    template <typename T, typename Value, typename ValueGetter>
+    std::size_t BinarySearch::LowerBound(const std::vector<T>& sortedData, const Value& value, ValueGetter getter)
+    {
+        return 0;
+    }
+
+    template <typename T, typename Value, typename ValueGetter>
+    std::size_t BinarySearch::UpperBound(const std::vector<T>& sortedData, const Value& value, ValueGetter getter)
+    {
+        return 0;
+    }
 }
--- src/core/algorithms/QuickSort.cpp
+++ src/core/algorithms/QuickSort.cpp
@@ -7,7 +7,33 @@ namespace FilmLibrary
 {
     // NOTE: QuickSort - шаблонный класс. Все методы определены в заголовочном файле.
     // если потребуется сократить время компиляции.
-
+    
     // TODO: Реализовать быструю сортировку.
     // template void QuickSort::Sort<Movie*, ...>(std::vector<Movie*>&, ...);
+    template <typename T, typename Comparator>
+    void QuickSort::Sort(std::vector<T>& data, Comparator comparator)
+    {
+    }
+
+    template <typename T, typename Comparator>
+    void QuickSort::SortRange(std::vector<T>& data, int low, int high, Comparator& comp)
+    {
+    }
+
+    template <typename T, typename Comparator>
+    int QuickSort::Partition(std::vector<T>& data, int low, int high, Comparator& comp)
+    {
+        return 0;
+    }
+
+    template <typename T, typename Comparator>
+    void QuickSort::InsertionSort(std::vector<T>& data, int low, int high, Comparator& comp)
+    {
+    }
+
+    template <typename T>
+    int QuickSort::MedianOfThree(std::vector<T>& data, int low, int high)
+    {
+        return 0;
+    }
 }
